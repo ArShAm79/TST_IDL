24fd3adaa417c4bcf0804269006a305f17790903
Arsham <arsham.ramezani@gmail.com>
2023-11-18 04:44:19 -0800

    fix: question 2 and 3 updated

--- 1.2.js
+++ 1.2.js
@@ -1,6 +1,24 @@
-Promise.resolve()
-  .then(() => console.log("Promise 1"))
-  .then(() => console.log("Promise 2"))
-  .then(() => console.log("Promise 3"));
+const promise1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("Promise 1 executed");
+    resolve();
+  }, 1000);
+});
+
+promise1.then(() => {
+  const promise2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Promise 2 executed");
+      resolve();
+    }, 500);
+  });
+
+  promise2
+    .then(() => {
+      console.log("Promise 2 chain executed 1");
+    })
+    .then(() => console.log("Promise 2 chain executed 2"))
+    .then(() => console.log("Promise 2 chain executed 3"));
+});
 
 console.log("Outside Promise");
--- 1.3.js
+++ 1.3.js
@@ -1,2 +1,6 @@
 const arr = [4, [[5], [6, [7], 8], 9, 10]];
-const flattened = arr.flat(Infinity);
+const toFlattedArray = (arr) => {
+  return arr.flat(Infinity);
+};
+
+console.log(toFlattedArray(arr));
